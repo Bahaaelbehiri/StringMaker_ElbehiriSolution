c479174729eeaea72c6d2f7c0f3ea3eb7238581d
Bahaa <Bahaa@DESKTOP-FA8A547>
2023-10-23 17:48:35 -0700

    Add project files.

---
+++ StringMaker_Elbehiri/Program.cs
@@ -0,0 +1,60 @@
+﻿// Bahaa Elbehiri
+// IT113
+
+using StringMaker_Elbehiri;
+using System.Reflection.Metadata;
+using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+namespace StringMaker_Elbehiri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringManager SM = new StringManager();    
+            string str = "Sunbeam Tiger";
+            Console.WriteLine(SM.Reverse(str));
+            Console.WriteLine(SM.ToString());
+            bool equals = str.Equals(str);
+            Console.WriteLine(equals);
+            Console.WriteLine(SM.Reverse(str, true));
+
+            str = "ABBA";
+            bool Sym = SM.Symmetric(str);
+            if (Sym)
+            {
+                Console.WriteLine(str + " is Symmetric");
+            }
+            else
+            {
+                Console.WriteLine(str + " is Not Symmetric");
+            }
+
+            str = "ABA";
+            bool Sym1 = SM.Symmetric(str);
+            if (Sym1)
+            {
+                Console.WriteLine(str + " is Symmetric");
+            }
+            else
+            {
+                Console.WriteLine(str + " is Not Symmetric");
+            }
+
+            str = "ABba";
+            bool Sym2 = SM.Symmetric(str);
+            if (Sym2)
+            {
+                Console.WriteLine(str + " is Symmetric");
+            }
+            else
+            {
+                Console.WriteLine(str + " is Not Symmetric");
+            }
+
+            str = "Dan";
+        }
+    }
+}
---
+++ StringMaker_Elbehiri/StringManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class StringManager
+{
+    private Stack<char> stack = new Stack<char>();
+
+    public string Reverse(string sm)
+    {
+        return Reverse(sm,false );
+    }
+
+    public string Reverse(string sm, bool PreserveCaseLocation)
+    {
+        stack.Clear();
+        Num = sm;
+        if (PreserveCaseLocation == false)
+        {
+            foreach (char c in sm)
+            {
+                stack.Push(c);
+            }
+        }
+        else
+        {
+            int revIndex = sm.Length - 1;
+            for (int i = 0; i < sm.Length; i++)
+            {
+                if (char.IsUpper(sm[revIndex]))
+                {
+                    stack.Push(char.ToUpper(sm[i]));
+                }
+                else
+                {
+                    stack.Push(char.ToLower(sm[i]));
+                }
+                revIndex--;
+            }
+        }
+        return new string(stack.ToArray());
+    }
+
+    public bool Symmetric(string sm)
+    {
+        Num = sm;
+        string reversedStr = Reverse(sm);
+        return sm==reversedStr;
+    }
+    public String Num = "";
+    public override string ToString()
+    {
+        int ASCII_NO = 0;
+        string myString = "";
+        foreach (char c in Num)
+        {
+            ASCII_NO += (int)c;
+        }
+        string nbr = "";
+        foreach (char c in ASCII_NO.ToString()) {
+            if(c > 0)
+            switch (c)
+            {
+                case '0':
+                    nbr += "Zero";
+                    break;
+                case '1':
+                    nbr += "One ";
+                    break;
+                case '2':
+                    nbr += "Two ";
+                    break;
+                case '3':
+                    nbr += "Three ";
+                    break;
+                case '4':
+                    nbr += "Four ";
+                    break;
+                case '5':
+                    nbr += "Five ";
+                    break;
+                case '6':
+                    nbr += "Six ";
+                    break;
+                case '7':
+                    nbr += "Seven ";
+                    break;
+                case '8':
+                    nbr += "Eight ";
+                    break;
+                case '9':
+                    nbr += "Nine ";
+                    break;
+            }
+            if (nbr!= string.Empty) 
+            {
+                return nbr;
+            }
+        }
+        
+        return "Negative One";
+    }
+}
